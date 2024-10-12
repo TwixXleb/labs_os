ac94272721929a40802761314ecfc8fd899720fe
TwixXleb <hackermanspacex@gmail.com>
2024-10-12 18:12:07 +0300

    bruh

--- src/child.c
+++ src/child.c
@@ -2,23 +2,6 @@
 #include <string.h>
 #include <ctype.h>
 
-int is_vowel(char c) {
-    c = tolower(c);
-    return (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'y');
-}
-
-void remove_vowels(char* str) {
-    int i, j = 0;
-    char result[1024];
-    for (i = 0; str[i] != '\0'; i++) {
-        if (!is_vowel(str[i])) {
-            result[j++] = str[i];
-        }
-    }
-    result[j] = '\0';
-    strcpy(str, result);
-}
-
 int main(int argc, char *argv[]) {
     if (argc < 2) {
         fprintf(stderr, "Usage: %s <output_file>\n", argv[0]);
