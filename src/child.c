02ac8ef0f9a8773ad76f4699c6003cfb51c30d4f
TwixXleb <hackermanspacex@gmail.com>
2024-10-12 19:58:42 +0300

    bruh

--- src/child.c
+++ src/child.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <string.h>
 #include "../include/utils.h"
 
 void child_process(int pipe_in, const char* file_name) {
@@ -24,4 +25,16 @@ void child_process(int pipe_in, const char* file_name) {
     }
 
     close(file_fd);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Использование: %s <output_file>\n", argv[0]);
+        return 1;
+    }
+
+    int pipe_in = STDIN_FILENO;  // Ввод из стандартного ввода
+    child_process(pipe_in, argv[1]);
+
+    return 0;
 }
