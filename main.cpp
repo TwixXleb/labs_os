c553e376cd87f24f4ddade05ce93ee259823def7
TwixXleb <hackermanspacex@gmail.com>
2024-11-20 19:41:55 +0300

    First lab remake

--- main.cpp
+++ main.cpp
@@ -1,12 +1,8 @@
 #include <stdio.h>
 #include "include/parent.h"
-#include "include/utils.h"
 
 int main() {
     char testStr[] = "Hello World";
-    printf("Пример работы remove_vowels:\n");
-    printf("До: %s\n", testStr);
-    remove_vowels(testStr);
     printf("После: %s\n", testStr);
     return 0;
 }
