884f9cbe43c85b53797290f6dccbb8f6aa292570
TwixXleb <hackermanspacex@gmail.com>
2024-11-22 19:13:08 +0300

    First lab remake 2.0

--- test/test.cpp
+++ test/test.cpp
@@ -1,37 +1,11 @@
 #include <gtest/gtest.h>
-#include <fstream>
-#include <string>
-#include <vector>
+#include <cstdlib>
 
-// Функция для чтения файла в вектор строк
-std::vector<std::string> read_file(const std::string& filename) {
-    std::ifstream file(filename);
-    std::vector<std::string> lines;
-    std::string line;
-    while (std::getline(file, line)) {
-        lines.push_back(line);
-    }
-    return lines;
-}
-
-// Тест для проверки, что строки без гласных записаны в правильные файлы
-TEST(ProcessTest, TestOutputFiles) {
-
+// Тест для проверки, что процессы корректно завершаются
+TEST(ProcessTest, TestProcessCompletion) {
     // Запускаем родительский процесс
-    system("./parent");
-
-    // Читаем output_1.txt и output_2.txt
-    auto output_1_lines = read_file("output_1.txt");
-    auto output_2_lines = read_file("output_2.txt");
-
-    // Проверяем, что строки без гласных записаны в правильные файлы
-    for (const auto& line : output_1_lines) {
-        EXPECT_EQ(line.find_first_of("aeiouAEIOU"), std::string::npos);
-    }
-
-    for (const auto& line : output_2_lines) {
-        EXPECT_EQ(line.find_first_of("aeiouAEIOU"), std::string::npos);
-    }
+    int status = system("./parent");
+    EXPECT_EQ(status, 0); // Убеждаемся, что процесс завершился без ошибок
 }
 
 int main(int argc, char **argv) {
