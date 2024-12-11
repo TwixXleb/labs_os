accdd0e64edb24c3eae14bf2670aedc0eb0a5f07
TwixXleb <144208870+TwixXleb@users.noreply.github.com>
2024-12-11 17:59:25 +0300

    Delete cmake-build-debug directory

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Projects/CLionProjects/labs_os
-# Build directory: D:/Projects/CLionProjects/labs_os/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("LW1")
-subdirs("tests")
