12e6b71841aa1b0ec0e7ac45f2b93db5a4f7feac
TwixXleb <hackermanspacex@gmail.com>
2024-12-12 15:03:18 +0300

    LAB_3 done

--- cmake-build-debug/tests/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Projects/CLionProjects/labs_os/tests
-# Build directory: D:/Projects/CLionProjects/labs_os/cmake-build-debug/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(LW4_test "LW4_test")
-set_tests_properties(LW4_test PROPERTIES  _BACKTRACE_TRIPLES "D:/Projects/CLionProjects/labs_os/tests/CMakeLists.txt;28;add_test;D:/Projects/CLionProjects/labs_os/tests/CMakeLists.txt;0;")
-subdirs("../_deps/googletest-build")
